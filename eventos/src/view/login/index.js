7d415302a32026cbd0f5390cdfee3807e602c58c
Edyomar Vargas <edyomar-vargas@hotmail.com>
2023-02-28 09:25:03 -0300

    feat: add fogot password and sign up buttons on login page

--- eventos/src/view/login/index.js
+++ eventos/src/view/login/index.js
@@ -1,20 +1,17 @@
+import "./login.css";
+
 function Login() {
   return (
     <div className="login-container d-flex align-items-center">
       <form className="mx-auto">
-        <img
-          className="mb-4"
-          src="/docs/5.3/assets/brand/bootstrap-logo.svg"
-          alt=""
-          width="72"
-          height="57"
-        />
-        <h1 className="h3 mb-3 fw-normal">Login</h1>
+        <h1 className="h3 mb-3 fw-normal text-white font-weight-bold text-center">
+          Login
+        </h1>
 
         <div className="form-floating">
           <input
             type="email"
-            className="form-control"
+            className="form-control my-2"
             id="floatingInput"
             placeholder="name@example.com"
           />
@@ -23,17 +20,32 @@ function Login() {
         <div className="form-floating">
           <input
             type="password"
-            className="form-control"
+            className="form-control my-2"
             id="floatingPassword"
             placeholder="Password"
           />
           <label for="floatingPassword">Password</label>
         </div>
 
-        <button className="w-100 btn btn-lg btn-primary" type="submit">
-          Login
+        <button className="w-100 btn btn-lg btn-login" type="submit">
+          Sign in
         </button>
-        <p className="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
+
+        <div className="msg-login text-white text-center my-4">
+          <span>Tudo certo! Você está conectado! &#128526;</span>
+          <br />
+          <span>Algo deu errado! Verifique o usuário ou a senha!</span>
+        </div>
+
+        <div className="login-options mt-4">
+          <a href="#" className="mx-2">
+            Recuperar senha
+          </a>
+          <span className="text-white">&#9733;</span>
+          <a href="#" className="mx-2">
+            Quero cadastrar
+          </a>
+        </div>
       </form>
     </div>
   );
