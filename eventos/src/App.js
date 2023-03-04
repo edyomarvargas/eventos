97315942fb853a3074a4cf4278f8f5c2ee2990b5
Edyomar Vargas <edyomar-vargas@hotmail.com>
2023-03-04 17:25:05 -0300

    feat: create home page

--- eventos/src/App.js
+++ eventos/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Login from './view/login';
 import Register from './view/register';
+import Home from './view/home';
 
 function App() {
   return (
     <Router>
       <Route exact path="/" component={Login} />
       <Route exact path="/register" component={Register} />
+      <Route exact path="/home" component={Home} />
     </Router>
   );
 }
---
+++ eventos/src/view/home/index.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import "./home.css";
+import { Link } from "react-router-dom";
+
+function Home() {
+  return <h1>Página inicial</h1>;
+}
+
+export default Home;
