dd7909476b007fa96e8b60f334b35ef7e49d2710
Edyomar Vargas <edyomar-vargas@hotmail.com>
2023-03-04 21:03:00 -0300

    feat: implement navbar for logged in users

--- eventos/src/view/components/navbar/index.js
+++ eventos/src/view/components/navbar/index.js
@@ -1,7 +1,9 @@
 import "./navbar.css";
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 
 function Navbar() {
+  const dispatch = useDispatch();
   return (
     <nav className="navbar navbar-expand-lg">
       <div className="container-fluid">
@@ -27,17 +29,45 @@ function Navbar() {
               </Link>
             </li>
 
-            <li className="nav-item">
-              <Link className="nav-link" aria-current="page" to="/register">
-                Cadastrar
-              </Link>
-            </li>
+            {useSelector((state) => state.isLogged) ? (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" aria-current="page" to="">
+                    Publicar evento
+                  </Link>
+                </li>
 
-            <li className="nav-item">
-              <Link className="nav-link" aria-current="page" to="/login">
-                Login
-              </Link>
-            </li>
+                <li className="nav-item">
+                  <Link className="nav-link" aria-current="page" to="">
+                    Meus eventos
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link
+                    className="nav-link"
+                    aria-current="page"
+                    onClick={() => dispatch({ type: "LOGOUT" })}
+                  >
+                    Sair
+                  </Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" aria-current="page" to="/register">
+                    Cadastrar
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link className="nav-link" aria-current="page" to="/login">
+                    Login
+                  </Link>
+                </li>
+              </>
+            )}
           </ul>
         </div>
       </div>
