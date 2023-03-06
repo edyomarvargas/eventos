81aeb0dc0f18f42b4199e37da54298517d2dbce1
Edyomar Vargas <edyomar-vargas@hotmail.com>
2023-03-06 08:48:22 -0300

    feat: setup firestore database

--- eventos/src/config/firebase.js
+++ eventos/src/config/firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAVBQZZM-p_QoI0y5yvOCVCEAsY0Xnk6iU",
@@ -14,3 +16,5 @@ const app = initializeApp(firebaseConfig);
 
 export default app;
 export const auth = getAuth();
+export const storage = getStorage(app);
+export const db = getFirestore(app);
--- eventos/src/view/createEvent/index.js
+++ eventos/src/view/createEvent/index.js
@@ -1,12 +1,48 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
 import Navbar from "../components/navbar";
+import { collection, addDoc } from "firebase/firestore";
+import { getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import { db } from "../../config/firebase";
 import "./createEvent.css";
-import firebase from "../../config/firebase";
 
 function CreateEvent() {
   const [msgType, setMsgType] = useState("");
+  const [title, setTitle] = useState("");
+  const [type, setType] = useState("");
+  const [description, setDescription] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [image, setImage] = useState("");
+  const userEmail = useSelector((state) => state.userEmail);
+
+  const createNewEvent = async () => {
+    setMsgType("");
+
+    try {
+      await addDoc(collection(db, "eventos"), {
+        title,
+        type,
+        description,
+        date,
+        time,
+        user: userEmail,
+        views: 0,
+        image: image.name,
+        isPublic: true,
+        created: new Date(),
+      });
+
+      const storage = getStorage();
+      const storageRef = ref(storage, `images/${image.name}`);
+      uploadBytesResumable(storageRef, image);
+
+      setMsgType("success");
+    } catch (error) {
+      setMsgType("error");
+    } 
+  };
+
   return (
     <>
       <Navbar />
@@ -19,12 +55,19 @@ function CreateEvent() {
         <form className="form-container">
           <div className="form-group">
             <label>Título:</label>
-            <input type="text" className="form-control" />
+            <input
+              type="text"
+              className="form-control"
+              onChange={(e) => setTitle(e.target.value)}
+            />
           </div>
 
           <div className="form-group">
             <label>Tipo do Evento:</label>
-            <select className="form-control">
+            <select
+              className="form-control"
+              onChange={(e) => setType(e.target.value)}
+            >
               <option disabled selected value>
                 -- Selecione um tipo --
               </option>
@@ -37,29 +80,46 @@ function CreateEvent() {
 
           <div className="form-group">
             <label>Descrição do evento:</label>
-            <textarea className="form-control" rows="3" />
+            <textarea
+              className="form-control"
+              rows="3"
+              onChange={(e) => setDescription(e.target.value)}
+            />
           </div>
 
           <div className="form-group row">
             <div className="col-6">
               <label>Data:</label>
-              <input type="date" className="form-control" />
+              <input
+                type="date"
+                className="form-control"
+                onChange={(e) => setDate(e.target.value)}
+              />
             </div>
 
             <div className="col-6">
               <label>Hora:</label>
-              <input type="time" className="form-control" />
+              <input
+                type="time"
+                className="form-control"
+                onChange={(e) => setTime(e.target.value)}
+              />
             </div>
           </div>
 
           <div className="form-group">
             <label>Imagem do evento:</label>
-            <input type="file" className="form-control" />
+            <input
+              type="file"
+              className="form-control"
+              onChange={(e) => setImage(e.target.files[0])}
+            />
           </div>
 
           <button
             type="button"
             className="btn btn-lg btn-block mt-3 mb-5 btn-register"
+            onClick={createNewEvent}
           >
             Criar Evento
           </button>
