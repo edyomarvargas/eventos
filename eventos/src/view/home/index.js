d6179964f199f18970987c5c03886f40c3b93cc8
Edyomar Vargas <edyomar-vargas@hotmail.com>
2023-03-07 09:10:40 -0300

    feat: render event component in home page

--- eventos/src/view/home/index.js
+++ eventos/src/view/home/index.js
@@ -1,16 +1,43 @@
-import { useState } from "react";
-import "./home.css";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Navbar from "../components/navbar";
 import EventCard from "../components/eventCard";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../config/firebase";
+import "./home.css";
 
 function Home() {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const eventsList = [];
+    const fetchEvents = async () => {
+      const querySnapshot = await getDocs(collection(db, "eventos"));
+      querySnapshot.forEach((doc) => {
+        eventsList.push({
+          id: doc.id,
+          ...doc.data(),
+        });
+      });
+      setEvents(eventsList);
+    };
+    fetchEvents();
+  }, []);
+
   return (
     <>
       <Navbar />
 
-      <div className="row">
-        <EventCard />
+      <div className="row p-4">
+        {events.map((event) => (
+          <EventCard
+            key={event.id}
+            id={event.id}
+            img={event.image}
+            title={event.title}
+            description={event.description}
+            views={event.views}
+          />
+        ))}
       </div>
     </>
   );
