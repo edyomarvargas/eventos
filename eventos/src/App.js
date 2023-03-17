57ab3484b7c70eda8b26633e263bbc314bb68d60
Edyomar Vargas <edyomar-vargas@hotmail.com>
2023-03-17 09:55:51 -0300

    feat: setup redux-persist

--- eventos/src/App.js
+++ eventos/src/App.js
@@ -5,22 +5,25 @@ import Home from "./view/home";
 import ForgotPassword from "./view/forgotPassword";
 import CreateEvent from "./view/createEvent";
 import EventDetails from "./view/eventDetails";
-import store from "./store";
+import { store, persistor } from "./store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 function App() {
   return (
     <Provider store={store}>
-      <Router>
-        <Route exact path="/" component={Home} />
-        <Route path="/events/:route" component={Home} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/forgot-password" component={ForgotPassword} />
-        <Route exact path="/create-event" component={CreateEvent} />
-        <Route path="/event-details/:id" component={EventDetails} />
-        <Route path="/edit-event/:id" component={CreateEvent} />
-      </Router>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <Route exact path="/" component={Home} />
+          <Route path="/events/:route" component={Home} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/forgot-password" component={ForgotPassword} />
+          <Route exact path="/create-event" component={CreateEvent} />
+          <Route path="/event-details/:id" component={EventDetails} />
+          <Route path="/edit-event/:id" component={CreateEvent} />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
--- eventos/src/store/index.js
+++ eventos/src/store/index.js
@@ -1,6 +1,16 @@
 import { createStore } from "redux";
 import userReducer from "./userReducer";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-const store = createStore(userReducer);
+const persistConfig = {
+  key: "siteeventos",
+  storage,
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, userReducer);
+
+const store = createStore(persistedReducer);
+const persistor = persistStore(store);
+
+export { store, persistor };
